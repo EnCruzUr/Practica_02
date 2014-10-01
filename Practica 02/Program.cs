f37add99f5f0bb89e00e5320d00def97f7ae0027
EnCruzUr <cruz_d.3@hotmail.com>
2014-09-30 19:18:18 -0500

    Acciones
    Se agregaron las opciones de lectura y escritura

--- Practica 02/Program.cs
+++ Practica 02/Program.cs
@@ -10,6 +10,30 @@ namespace Practica_02
     {
         public static void Acciones()
         {
+            int P1,P2,P3;
+            double V1,V2,V3;
+            string D1,D2,D3;
+            Console.WriteLine("Dame la cantidad del primer producto");
+            P1=int.Parse(Console.ReadLine());
+            Console.WriteLine("Dame la descripcion");
+            D1= Console.ReadLine();
+            Console.WriteLine("Dame el precio unitario");
+            V1=int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Dame la cantidad del segundo producto");
+            P2 = int.Parse(Console.ReadLine());
+            Console.WriteLine("Dame la descripcion");
+            D2 = Console.ReadLine();
+            Console.WriteLine("Dame el precio unitario");
+            V2 = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Dame la cantidad del tercer producto");
+            P3 = int.Parse(Console.ReadLine());
+            Console.WriteLine("Dame la descripcion");
+            D3 = Console.ReadLine();
+            Console.WriteLine("Dame el precio unitario");
+            V3 = int.Parse(Console.ReadLine());
+
 
         }
         static void Main(string[] args)
