28dfb9736bae3221d17e1d5df6d366e973fb0b6d
EnCruzUr <cruz_d.3@hotmail.com>
2014-09-30 19:09:22 -0500

    Proj_1
    Agregados archivos c#

---
+++ Practica 02/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Practica_02
+{
+    class Program
+    {
+        public static void Acciones()
+        {
+
+        }
+        static void Main(string[] args)
+        {
+            Acciones();
+        }
+    }
+}
