a7d4b694f0d7835340535da062dfa280be6b4404
EnCruzUr <cruz_d.3@hotmail.com>
2014-10-02 19:29:40 -0500

    Correcion Codigo Limpio
    Se realizo una limpieza para un visual al programador mas limpia.

---
+++ Practica 02/Productos.cs
@@ -0,0 +1,76 @@
+﻿using System;
+
+namespace Practica_02
+{
+    public class Productos
+    {
+        private int P1, P2, P3;
+        private double V1, V2, V3, R1, R2, R3, R4, R5, IVA;
+        private string D1, D2, D3;
+        public Productos()
+        {
+
+        }
+
+        public void realizarCapturaDeTresProductos()
+        {
+            this.capturarPrimerProducto();
+            this.capturarSegundoProducto();
+            this.capturarTercerProducto();
+            this.realizarOperaciones();
+            this.imprimirTablaDeResultados();
+        }
+
+        private void capturarPrimerProducto()
+        {
+            Console.WriteLine("Dame la cantidad del primer producto");
+            this.P1 = int.Parse(Console.ReadLine());
+            Console.WriteLine("Dame la descripcion");
+            this.D1 = Console.ReadLine();
+            Console.WriteLine("Dame el precio unitario");
+            this.V1 = double.Parse(Console.ReadLine());
+        }
+
+        private void capturarSegundoProducto()
+        {
+            Console.WriteLine("\n\nDame la cantidad del segundo producto");
+            this.P2 = int.Parse(Console.ReadLine());
+            Console.WriteLine("Dame la descripcion");
+            this.D2 = Console.ReadLine();
+            Console.WriteLine("Dame el precio unitario");
+            this.V2 = double.Parse(Console.ReadLine());
+        }
+
+        private void capturarTercerProducto()
+        {
+            Console.WriteLine("\n\nDame la cantidad del tercer producto");
+            this.P3 = int.Parse(Console.ReadLine());
+            Console.WriteLine("Dame la descripcion");
+            this.D3 = Console.ReadLine();
+            Console.WriteLine("Dame el precio unitario");
+            this.V3 = double.Parse(Console.ReadLine());
+        }
+        private void realizarOperaciones()
+        {
+            this.R1 = this.P1 * this.V1;
+            this.R2 = this.P2 * this.V2;
+            this.R3 = this.P3 * this.V3;
+            this.R4 = this.R1 + this.R2 + this.R3;
+            this.IVA = (this.R4 * 16) / 100;
+            this.R5 = this.R4 + this.IVA;
+        }
+        private void imprimirTablaDeResultados()
+        {
+            Console.WriteLine("\n\n\n------------------------");
+            Console.WriteLine(" " + this.P1 + "\t" + this.D1 + "\t" + this.R1 + "\t+");
+            Console.WriteLine(" " + this.P2 + "\t" + this.D2 + "\t" + this.R2 + "\t+");
+            Console.WriteLine(" " + this.P3 + "\t" + this.D3 + "\t" + this.R3 + "\n------------------------");
+            Console.WriteLine("Subtotal \t" + this.R4);
+            Console.WriteLine("Iva \t\t" + this.IVA);
+            Console.WriteLine("Total \t\t" + this.R5);
+            Console.WriteLine("Gracias Por Su Compra");
+            Console.WriteLine("\n\n\nPresione una tecla para salir");
+            Console.ReadKey(true);
+        }
+    }
+}
--- Practica 02/Program.cs
+++ Practica 02/Program.cs
@@ -8,52 +8,10 @@ namespace Practica_02
 {
     class Program
     {
-        public static void Acciones()
-        {
-            int P1,P2,P3;
-            double V1,V2,V3,R1,R2,R3,R4,R5,IVA;
-            string D1,D2,D3;
-
-            Console.WriteLine("Dame la cantidad del primer producto");
-            P1=int.Parse(Console.ReadLine());
-            Console.WriteLine("Dame la descripcion");
-            D1= Console.ReadLine();
-            Console.WriteLine("Dame el precio unitario");
-            V1=double.Parse(Console.ReadLine());
-
-            Console.WriteLine("\n\nDame la cantidad del segundo producto");
-            P2 = int.Parse(Console.ReadLine());
-            Console.WriteLine("Dame la descripcion");
-            D2 = Console.ReadLine();
-            Console.WriteLine("Dame el precio unitario");
-            V2 = double.Parse(Console.ReadLine());
-
-            Console.WriteLine("\n\nDame la cantidad del tercer producto");
-            P3 = int.Parse(Console.ReadLine());
-            Console.WriteLine("Dame la descripcion");
-            D3 = Console.ReadLine();
-            Console.WriteLine("Dame el precio unitario");
-            V3 = double.Parse(Console.ReadLine());
-
-            R1 = P1 * V1;
-            R2 = P2 * V2;
-            R3 = P3 * V3;
-            R4 = R1 + R2 + R3;
-            IVA = (R4 *16) / 100;
-            R5 = R4 + IVA;
-            Console.WriteLine("\t" + P1 + "\t" + D1 + "\t" + R1 + "\t+");
-            Console.WriteLine("\t" + P2 + "\t" + D2 + "\t" + R2 + "\t+");
-            Console.WriteLine("\t" + P3 + "\t" + D3 + "\t" + R3 + "\n------------------------");
-            Console.WriteLine("Subtotal "+R4);
-            Console.WriteLine("Iva "+IVA);
-            Console.WriteLine("Total "+R5);
-            Console.WriteLine("Gracias Por Su Compra");
-            Console.WriteLine("\n\n\nPresione una tecla para salir");
-            Console.ReadKey(true);
-        }
         static void Main(string[] args)
         {
-            Acciones();
+            Productos productos = new Productos();
+            productos.realizarCapturaDeTresProductos();
         }
     }
 }
